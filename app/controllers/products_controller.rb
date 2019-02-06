06600942ecc10e4f72b9d67513c51160c2f5eee0
nvaldebenito <nvaldebenito@localhost.localdomain>
2019-02-06 19:32:21 -0300

    add controler

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+	
+	def create
+		@product = Product.new(product_params)
+		@product.category = Category.find(params[:category_id])
+		@product.save
+		redirect_to @products.category
+	end
+
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :price)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :category
-  resources :products, only: [:create]
+  resources :categories do
+   	resources :products, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'students#index'
+  root 'categories#index'
 end
